5b600391ba865c4d7c8d30c626069d1aa1028d92
lishm <5201314dan>
2022-03-09 19:15:54 +0800

    init

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ astroer
 This project is a starting point for a Flutter
 [plug-in package](https://flutter.dev/developing-packages/),
 a specialized package that includes platform-specific implementation code for
-Android and/or iOS.
+Android and/or iOS.ddd
 
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
--- android/src/main/java/com/iusworks/astroer/AstroerPlugin.java
+++ android/src/main/java/com/iusworks/astroer/AstroerPlugin.java
@@ -24,7 +24,7 @@ public class AstroerPlugin implements FlutterPlugin, MethodCallHandler {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
+    /// when the Flutter Engine is detached from the Activitydd
     private MethodChannel channel;
 
     @Override
